943cefb1ae97304fca25fbef7df2d8a31b39531e
Nguyen Dai.dev <ngtrdai290197@gmail.com>
2020-02-09 01:32:16 +0700

    update graphql.module

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,26 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloModule, APOLLO_OPTIONS, Apollo } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { WebSocketLink } from 'apollo-link-ws';
 
 import { environment as env } from '@env/environment';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import { LocalStorageService } from './core/services';
 
 @NgModule({
-  imports: [BrowserModule, ApolloModule, HttpLinkModule],
-  providers: [
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: (httpLink: HttpLink) => {
-        return {
-          cache: new InMemoryCache(),
-          link: httpLink.create({
-            uri: env.GRAPHQL
-          })
-        };
-      },
-      deps: [HttpLink]
-    }
-  ]
+  imports: [BrowserModule, ApolloModule, HttpLinkModule]
 })
-export class GraphqlModule {}
+export class GraphqlModule {
+  constructor(
+    private apollo: Apollo,
+    private httpLink: HttpLink,
+    private localStorageService: LocalStorageService
+  ) {
+    const http = this.httpLink.create({
+      uri: `${env.GRAPHQL}`
+    });
+
+    // Create a WebSocket link:
+    const ws = new WebSocketLink({
+      uri: `ws://localhost:3000/graphql`,
+      options: {
+        reconnect: true,
+        connectionParams: () => ({
+          Authorization: `Bearer ${this.localStorageService.getToken()}`
+        })
+      }
+    });
+
+    const link = split(
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+          definition.kind === 'OperationDefinition' &&
+          definition.operation === 'subscription'
+        );
+      },
+      ws,
+      http
+    );
+    this.apollo.create({
+      link,
+      cache: new InMemoryCache()
+    });
+  }
+}
